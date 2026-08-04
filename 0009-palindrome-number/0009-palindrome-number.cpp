98642f2ef53c2372f8497556618f6c27dac53599
Sameer Gandhi <sm0031625@gmail.com>
2026-08-04 22:06:46 +0530

    Time: 5 ms (19.08%), Space: 8.4 MB (92.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+bool check(int x,int *temp){
+    if(x>=0 && x<=9){
+        int last=*temp%10;
+        *temp/=10;
+        return last==x;
+    }
+    bool result=check(x/10,temp) and x%10 == (*temp)%10;
+    *temp/=10;
+    return result;
+}
+    bool isPalindrome(int x) {
+        if(x<0){
+            return 0;
+        }
+        int temp=x;
+    bool result=check(x,&temp);
+    if(result){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+    }
+};
