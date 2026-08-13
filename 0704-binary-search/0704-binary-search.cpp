49432a75701b2689f99cffacf09d4e79ad5b805d
Sameer Gandhi <sm0031625@gmail.com>
2026-08-13 14:18:28 +0530

    Time: 0 ms (100%), Space: 31.3 MB (82.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,8 +3,8 @@ public:
     int search(vector<int>& nums, int target) {
         int low=0;
         int high=nums.size()-1;
-        int mid=low+(high-low)/2;
         while(low<=high){
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
@@ -14,7 +14,6 @@ public:
             else{
                 high=mid-1;
             }
-            mid=low+(high-low)/2;
         }
         return -1;
     }
