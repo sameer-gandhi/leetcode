5b98f0662cff8c897e2a567a74f74f4eb59f632b
Sameer Gandhi <sm0031625@gmail.com>
2026-08-13 17:43:02 +0530

    Time: 0 ms (100%), Space: 12.8 MB (6.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -20,7 +20,7 @@ public:
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
-            if(nums[mid]>nums[mid-1]){
+            else if(nums[mid]>nums[mid-1]){
                 low=mid+1;
             }
             else{
