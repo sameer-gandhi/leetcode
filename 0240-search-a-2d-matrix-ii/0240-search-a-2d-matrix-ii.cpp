c8759f2a4ca4c26af2aebf9becc2a9324170faff
Sameer Gandhi <sm0031625@gmail.com>
2026-08-18 11:32:54 +0530

    Time: 48 ms (78.4%), Space: 18.7 MB (69.24%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,20 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            int low=0;
-            int high=m-1;
-            while(low<=high){
-                int mid=low+(high-low)/2;
-                if(matrix[i][mid]==target){
-                    return true;
-                }
-                else if(matrix[i][mid]<target){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+        int row=0;
+        int col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
             }
         }
         return false;
