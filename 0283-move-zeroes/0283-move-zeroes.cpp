c12fcc2d8489457e65674082173de570e002ebe3
Sameer Gandhi <sm0031625@gmail.com>
2026-08-20 12:32:38 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,24 +3,22 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
         int j=-1;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             if(nums[i]==0){
                 j=i;
                 break;
             }
         }
-        if(j==-1) return;
-        int i=j+1;
-        while(i<n){
-            if(nums[i]!=0){
-                swap(nums[i],nums[j]);
-                i++;
-                j++;
-            }
-            else{
+        if(j!=-1){
+            int i=j+1;
+            while(i<n){
+                if(nums[i]!=0){
+                    swap(nums[j],nums[i]);
+                    j++;
+                }
                 i++;
             }
         }
-        
+        return;
     }
 };
