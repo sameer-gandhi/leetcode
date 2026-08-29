441bac239df59e425a6f975cefa3168c5fab34b0
Sameer Gandhi <sm0031625@gmail.com>
2026-08-29 21:12:57 +0530

    Time: 47 ms (35.84%), Space: 19.4 MB (23.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // if(s.empty()){
-        //     return 0;
-        // }
-        // int mxlen=INT_MIN;
-        // for(int i=0;i<s.length();i++){
-        //     unordered_map<char,int> mp;
-        //     int len=0;
-        //     for(int j=i;j<s.length();j++){
-        //         bool check=false;
-        //         len++;
-        //         mp[s[j]]++;
-        //         for(auto el:mp){
-        //             if(el.second>1){
-        //                 check=true;
-        //                 break;
-        //             }
-        //         }
-        //         if(!check){
-        //             mxlen=max(len,mxlen);
-        //         }
-        //         else{
-        //             break;
-        //         }
-        //     }
-        // }
-        // return mxlen;
         if(s.empty()){
             return 0;
         }
@@ -38,20 +12,27 @@ public:
         while(i<=j && j<s.length()){
             len++;
             mp[s[j]]++;
-            bool check=false;
-            for(auto el:mp){
-                if(el.second>1){
-                    len--;
-                    mp[s[i]]--;
-                    i++;
-                    check=true;
-                    break;
-                }
+            while(mp[s[j]]>1){
+                len--;
+                mp[s[i]]--;
+                i++;
             }
-            if(!check){
-                mxlen=max(len,mxlen);
-            }
+            mxlen=max(len,mxlen);
             j++;
+            // bool check=false;
+            // for(auto el:mp){
+            //     if(el.second>1){
+            //         len--;
+            //         mp[s[i]]--;
+            //         i++;
+            //         check=true;
+            //         break;
+            //     }
+            // }
+            // if(!check){
+            //     mxlen=max(len,mxlen);
+            // }
+            //j++;
         }
         return mxlen;
     }
