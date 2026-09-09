61d0deb78fe8a1229fd12b20f08b2f5e1190c805
Sameer Gandhi <sm0031625@gmail.com>
2026-09-09 20:59:01 +0530

    Time: 4 ms (1.33%), Space: 9.1 MB (57.33%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if(n/1000==0){
+            return 0;
+        }
+        long long count=0;
+        for(long long p=1000;p<=n;p*=1000){
+            count+=n-p+1;
+        }
+        return count;
+    }
+};
