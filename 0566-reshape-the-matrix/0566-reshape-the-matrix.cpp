cb5391116606a13eadd65f66cebcd0715ac7906e
Sameer Gandhi <sm0031625@gmail.com>
2026-09-15 20:22:11 +0530

    Time: 0 ms (100%), Space: 15.2 MB (26.99%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m=mat.size();
+        int n=mat[0].size();
+        if(r*c!=m*n){
+            return mat;
+        }
+        vector<vector<int>> result(r,vector<int>(c));
+        int idx=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                result[idx/c][idx%c]=mat[i][j];
+                idx++;
+            }
+        }
+        return result;
+    }
+};
